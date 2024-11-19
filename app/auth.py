018aff1620d54938bce8ff55957e83083906f329
Stefan Szepe <szepe@mdw.ac.at>
2024-11-19 09:46:45 +0100

    add comments

--- app/auth.py
+++ app/auth.py
@@ -6,7 +6,10 @@ from flask import abort, current_app
 
 
 def verify_token(token):
-    """Verify the access token with Keycloak's introspection endpoint."""
+    """
+    Verify the access token with Keycloak's introspection endpoint.
+    """
+    # Get the Keycloak introspection endpoint and client credentials
     introspect_url = current_app.config["KEYCLOAK_INTROSPECT_URI"]
     client_id = current_app.config["OIDC_CLIENT_ID"]
     client_secret = current_app.config["OIDC_CREDENTIALS_SECRET"]
@@ -19,10 +22,13 @@ def verify_token(token):
         headers={"Content-Type": "application/x-www-form-urlencoded"},
     )
 
+    # Check if the request was successful otherwise raise an error
     if response.status_code != 200:
         abort(500, description="Error connecting to authentication server")
 
+    # Parse the response from Keycloak
     token_data = response.json()
+    # Check if the token is active
     if not token_data.get("active", False):
         abort(401, description="Invalid or expired token")
 
