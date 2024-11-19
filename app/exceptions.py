5d38b85ca1e5a558548a174c61f3c5187478310b
Stefan Szepe <szepe@mdw.ac.at>
2024-11-19 12:51:16 +0100

    add custom TokenError exception

---
+++ app/exceptions.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+class TokenError(Exception):
+    def __init__(self, message, status_code):
+        super().__init__(message)
+        self.status_code = status_code
+        
