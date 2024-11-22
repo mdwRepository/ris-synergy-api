3daddfa2de9172511cafee2c409ed58d458a3523
Stefan Szepe <szepe@mdw.ac.at>
2024-11-22 08:54:26 +0100

    fix W0613: Unused argument 'error'

--- app/error_handlers.py
+++ app/error_handlers.py
@@ -9,33 +9,33 @@ def register_error_handlers(app):
     """
 
     @app.errorhandler(500)
-    def internal_server_error(error):
+    def internal_server_error(_):
         return jsonify(error="500 Error: Internal server error"), 500
 
     @app.errorhandler(403)
-    def forbidden(error):
+    def forbidden(_):
         return jsonify(error="403 Error: Forbidden"), 403
 
     @app.errorhandler(404)
-    def not_found(error):
+    def not_found(_):
         return jsonify(error="404 Error: Page not found"), 404
 
     @app.errorhandler(405)
-    def method_not_allowed(error):
+    def method_not_allowed(_):
         return jsonify(error="405 Error: Method not allowed"), 405
 
     @app.errorhandler(400)
-    def bad_request(error):
+    def bad_request(_):
         return jsonify(error="400 Error: Bad request"), 400
 
     @app.errorhandler(401)
-    def unauthorized(error):
+    def unauthorized(_):
         return jsonify(error="401 Error: Unauthorized"), 401
 
     @app.errorhandler(429)
-    def too_many_requests(error):
+    def too_many_requests(_):
         return jsonify(error="429 Error: Too many requests"), 429
 
     @app.errorhandler(503)
-    def service_unavailable(error):
+    def service_unavailable(_):
         return jsonify(error="503 Error: Service unavailable"), 503
